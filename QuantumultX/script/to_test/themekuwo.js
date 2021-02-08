c6608c525b0cb7d3b0bf0451bb124b0a7548052f
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-08 15:29:43 +0800

    Create themekuwo.js

---
+++ QuantumultX/script/to_test/themekuwo.js
@@ -0,0 +1,11 @@
+/*
+#酷我换肤(已经有的皮肤需要先从本地皮肤删除再换 By@ s y)
+^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/theme)  url script-response-body   https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/themekuwo.js
+
+vip1.kuwo.cn
+
+var obj = JSON.parse($response.body);
+ obj.data.vipTheme.type="free";
+ obj.data.needBieds=null;
+$done({body: JSON.stringify(obj)});
+//
