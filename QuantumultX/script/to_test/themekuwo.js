7f367ff1a85351db9b2c870142e221fc1ba88d2d
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-08 15:30:05 +0800

    Update themekuwo.js

--- QuantumultX/script/to_test/themekuwo.js
+++ QuantumultX/script/to_test/themekuwo.js
@@ -3,7 +3,7 @@
 ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/theme)  url script-response-body   https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/themekuwo.js
 
 vip1.kuwo.cn
-
+*/
 var obj = JSON.parse($response.body);
  obj.data.vipTheme.type="free";
  obj.data.needBieds=null;
