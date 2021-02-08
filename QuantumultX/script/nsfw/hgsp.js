30fc7764e40b049b10b391fb7dde639fdde011ca
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-08 15:18:11 +0800

    cucumber

--- QuantumultX/script/nsfw/hgsp.js
+++ QuantumultX/script/nsfw/hgsp.js
@@ -1,21 +1,36 @@
-/*黄瓜视频无限观影 +  解锁付费影片
-#基于jungegeCN脚本修改
+/*
 
-黄瓜视频tf版下载地址：http://suo.im/5Fc3Ls
+#黄瓜视频完美🔓 by JungegeCN
 
-# by微信公众号：少年歌行PRO 2020/9/4 更新
-# 电报资源发布群（禁言纯净）@shao66 
-# 电报水友交流群（资源同步）@sngxpro
-# 电报私聊 @shaogebot
+下载地址：https://849slfm.bpwbel.work/vfr1273.html?pkg=cuke0001&accountCode=666&inviteCode=AQBNNV
+
+电报频道：https://t.me/ThorHCC
+QQ资源群：189519867
+
+资源发布：https://www.notion.so/7a95c49260584ac8b438e18bf892531b
+
 
 圈X：
-https:\/\/api1000\.cvxia\.com\/(user\/info|mov\/browse2*) url script-response-body https://raw.githubusercontent.com/JungegeCN/JGG/master/hgsp.js
 
-MITM = api1000.cvxia.com
+https:\/\/api1000\.gdqeb\.club\/(user\/info|mov\/browse2*) url script-response-body https://raw.githubusercontent.com/JungegeCN/JGG/master/hgsp.js
+
+Loon:
+[Script]
+#黄瓜视频VIP
+http-response ^https:\/\/api88\.awk2\.work\/(user\/info|mov\/browse2*) script-path=https://raw.githubusercontent.com/JungegeCN/JGG/master/hgsp.js, requires-body=true, timeout=10, tag=黄瓜视频VIP
+
+
+MITM = api1000.gdqeb.club
+
+
 */
 
 var body = $response.body;
 
+
+
+
 body=body.replace(/vipLogo\":\d/g,'vipLogo":4').replace(/gender\":(\-|\+?)\d+/g,'gender":1').replace(/isPaid\":\d+/g,'isPaid":1').replace(/supUserId\":\d+/g,'supUserId":0').replace(/hasBuy\":\w+/g,'hasBuy":true').replace(/vipEndDate\":\"(.*?)\"/g,'vipEndDate":\"2222-02-22\"').replace(/phone\":\"(.*?)\"/g,'phone":\"JungegeCN\"').replace(/userCls\":\d+/g,'userCls":2').replace(/isExpired\":\d+/g,'isExpired":1');
+
 
 $done({body});
