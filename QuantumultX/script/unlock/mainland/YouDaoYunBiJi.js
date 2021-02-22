c1b33602916f057a5000eda3d49c77b794118fcc
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-22 13:06:22 +0800

    note ( expire 2029.03.04 )

--- QuantumultX/script/unlock/mainland/YouDaoYunBiJi.js
+++ QuantumultX/script/unlock/mainland/YouDaoYunBiJi.js
@@ -1,6 +1,6 @@
 /*
-有道云笔记VIP功能 
-       By Alex0510
+有道云笔记VIP功能  By Alex0510
+
 Surge 4:
 http-response https://note.youdao.com/yws/(mapi/payment|api/self) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Alex0510/surge/master/Script/ydybj.js
 
