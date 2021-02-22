9ceb54ba706a6501d0cf152f96b2f9ccb0d986c5
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-22 13:03:46 +0800

    draft & mindmap

--- QuantumultX/script/unlock/mainland/MuBu.js
+++ QuantumultX/script/unlock/mainland/MuBu.js
@@ -1,9 +1,9 @@
 /*
 # 幕布 @ZhiYi-N
 #loon
-http-response https:\/\/api2\.mubu\.com\/v3\/api\/user\/current_user script-path=https://raw.githubusercontent.com/Redeembynight/own/main/QuantumultX/script/unlock/mainland/mb.js, requires-body=true, timeout=10, tag=幕布
+http-response https:\/\/api2\.mubu\.com\/v3\/api\/user\/current_user script-path=https://raw.githubusercontent.com/Redeembynight/own/main/QuantumultX/script/unlock/mainland/MuBu.js, requires-body=true, timeout=10, tag=幕布
 #QuanX
-https:\/\/api2\.mubu\.com\/v3\/api\/user\/current_user url script-response-body https://raw.githubusercontent.com/Redeembynight/own/main/QuantumultX/script/unlock/mainland/mb.js
+https:\/\/api2\.mubu\.com\/v3\/api\/user\/current_user url script-response-body https://raw.githubusercontent.com/Redeembynight/own/main/QuantumultX/script/unlock/mainland/MuBu.js
 
 #mitm=api2.mubu.com
 */
