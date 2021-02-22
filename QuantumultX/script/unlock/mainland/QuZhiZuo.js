d136a672e1f3ae7a1130481f4c8152bcd4970bfa
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-22 15:33:29 +0800

    edit videos ( expire 2029.10.10 )

--- QuantumultX/script/unlock/mainland/QuZhiZuo.js
+++ QuantumultX/script/unlock/mainland/QuZhiZuo.js
@@ -1,6 +1,9 @@
 /*
+#趣制作 解锁会员模板视频（20200925）Attention：登录使用
 Unlocks by Cuttlefish 公众号：墨鱼手记
-# > 033 趣制作 解锁会员模板视频（20200925）Attention：登录使用
+
+下载地址 https://apps.apple.com/cn/app/%E8%B6%A3%E8%A7%86%E9%A2%91%E5%88%B6%E4%BD%9C-%E8%A7%86%E9%A2%91%E7%BC%96%E8%BE%91-%E8%A7%86%E9%A2%91%E6%8A%A0%E5%9B%BE-%E5%89%AA%E8%BE%91%E5%B7%A5%E5%85%B7/id1449242864
+
 https://cm.szsszykj.com/interface/GetVip.php url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/qzz.js
 
 cm.szsszykj.com, 
