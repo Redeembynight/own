1c4fe06dce32e1f60e2f42c539ab95f1844070ce
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-22 12:55:09 +0800

    Create hjsp.js

---
+++ QuantumultX/script/unknow/hjsp.js
@@ -0,0 +1,11 @@
+
+/*
+# 花椒视频 (By Alex0510)
+http://user.shywck.com/user/userinfo url script-response-body https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/hjsp.js
+
+user.shywck.com
+*/
+
+
+body = $response.body.replace(/.+/, "r2E18C9eded9yAup6iRND3cJpuvlH0xMj3haW7LLQ2Ql3QH3Ugtzp+puIMQNDGNAuYHySilwAzFklM4SIn/AqrOf6MzJCQZ43+PsMSgtnGYceWLgqScXVCwXFQXKR0Cxm7uDE1nEz+hfJnKq9oD6sgNiz7KibxRv1n1mQyXUPkCusDvhverqxX73YhJ5EWJGt5ITAcPg8WeE7FDPFRLvbA==")
+$done({body});
